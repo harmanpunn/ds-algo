0ace85065acc9689f3b5a79e4e591b329cdd59de
832022 <harmanpreet.singh3@ihg.com>
2020-10-19 00:06:07 +0530

    Leet Code - Remove Duplicates

---
+++ learning-datastructures/src/code/practice/RemoveDuplicates.java
@@ -0,0 +1,32 @@
+package code.practice;
+
+public class RemoveDuplicates {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int nums[] = {0,0,1,1,1,2,2,3,3,4};
+		int len = removeDuplicates(nums);	
+		
+		for(int i=0; i<len; i++) {
+			System.out.println(nums[i]);
+		}
+		
+
+	}
+	
+	public static int removeDuplicates(int[] nums) {
+        if(nums.length == 0) return 0;
+        
+        int i=0;
+        for(int j=1;j<nums.length;j++) {
+            if(nums[j] != nums[i]) {
+                i++;
+                nums[i] = nums[j];
+            }
+        }
+        
+        return i+1;
+    }
+
+}
